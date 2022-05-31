6eb006be9974174f75d1404afb0cdf50540cae33
s1091828 <s1091828@gm.pu.edu.tw>
2022-05-31 10:07:09 +0800

    v5: 發射及音效

--- app/src/main/java/com/example/shootgame2022/MySurfaceView.kt
+++ app/src/main/java/com/example/shootgame2022/MySurfaceView.kt
@@ -17,6 +17,7 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
     var BGmoveX:Int = 0
     var fly:Fly
     var gDetector: GestureDetector
+    var mper: MediaPlayer
 
     init {
         surfaceHolder = getHolder()
@@ -24,6 +25,7 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
         surfaceHolder.addCallback(this)
         fly = Fly(context!!)
         gDetector = GestureDetector(context, this)
+        mper = MediaPlayer()
     }
 
     override fun surfaceCreated(p0: SurfaceHolder) {
@@ -73,8 +75,12 @@ class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
         return true
     }
 
-    override fun onShowPress(p0: MotionEvent?) {
-
+    override fun onShowPress(e: MotionEvent?) {
+        if (e!!.x >= 0 && e!!.x <= fly.w && e!!.y >= fly.y && e!!.y <= fly.y + fly.w) {
+            fly.fire = 1
+            mper = MediaPlayer.create(context, R.raw.shoot)
+            mper.start()
+        }
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
